bd9354334c8a3a81cae0f8696dc82d4e334da356
Shrutibarade <shrutibarade9160@gmail.com>
2025-01-31 23:57:05 +0530

    Feat:  Using Xml json student object get

--- src/main/java/org/dnyanyog/controller/StudentController.java
+++ src/main/java/org/dnyanyog/controller/StudentController.java
@@ -1,11 +1,13 @@
 package org.dnyanyog.controller;
 
-import java.util.List;
+import org.dyanyog.dto.Student;
 import org.dyanyog.service.StudentService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -23,9 +25,20 @@ public class StudentController {
         return studentService.getLastName();
     }
 
-    @GetMapping("/email/{roll_number}")
+    @GetMapping("/email/{roll_number}") // Fixed path and added roll_number parameter
     public String getEmail(@PathVariable int roll_number) {
         return studentService.getEmail(roll_number);
+    }
+
+    @GetMapping(value = "/student", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    public Student getStudent() {
+        Student std = new Student();
+        std.setFirstName("Vaibhav");
+        std.setLastName("Jodge");
+        std.setEmail("aj287");
+        std.setMobile("12121212");
+        std.setAge(81);
+        return std;
     }
 
     @GetMapping("/mobile/{roll_number}")
